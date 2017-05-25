46d3bd1b3495bc071120513ee7bb0df876304d5b
Don Nguyen <don.nguyen@hazuu.com>
2017-05-25 11:11:18 +0700

    Add Gulpfile to support dev

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+
+
+gulp.task('styles', function(){
+  gulp.src(['sass/**/*.scss'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('dist/styles/'))
+});
+
+
+gulp.task('default', function(){
+  gulp.watch("sass/**/*.scss", ['styles']);
+});
